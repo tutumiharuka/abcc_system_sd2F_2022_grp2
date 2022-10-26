129c786772bee4794b493faf0bf3c29473a6feff
シン　エイジュ <48903619+an9us@users.noreply.github.com>
2022-10-26 12:14:50 +0900

    karapo.htmlを追加した
    karapoは、空っぽのBOOTSTRAPのファイルです、<body>の中にbootstrapを書いてください

--- Navbar.php
+++ Navbar.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="#">Navbar</a>
@@ -34,3 +44,7 @@
     </div>
   </div>
 </nav>
+
+</body>
+</html>
+
