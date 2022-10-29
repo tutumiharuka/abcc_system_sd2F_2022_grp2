8f5ac6ec864cd5d16ee5bee99ac3a1efec28bdce
シンエイジュ <48903619+an9us@users.noreply.github.com>
2022-10-29 22:53:45 +0900

    TOPページの最新作までに

--- G1-1_Top.php
+++ G1-1_Top.php
@@ -6,18 +6,134 @@
 
 
 <div class="carousel mt-3">
-  <div class="px-1"><img src="img/SPO/SPO_b01.jpg" alt="" class="carousel_img rounded"></div>
-  <div class="px-1"><img src="img/SPO/SPO_b02.jpg" alt="" class="carousel_img rounded"></div>
-  <div class="px-1"><img src="img/SPO/SPO_b03.jpg" alt="" class="carousel_img rounded"></div>
-  <div class="px-1"><img src="img/SPO/SPO_b04.jpg" alt="" class="carousel_img rounded"></div>
-  <div class="px-1"><img src="img/FIG/FIG_b01.jpg" alt="" class="carousel_img rounded"></div>
-  <div class="px-1"><img src="img/FIG/FIG_b02.jpg" alt="" class="carousel_img rounded"></div>
-  <div class="px-1"><img src="img/FIG/FIG_b03.jpg" alt="" class="carousel_img rounded"></div>
+  <div class="px-1 carousel-card"><img src="img/SPO/SPO_b01.jpg" alt="" class="carousel-img rounded"></div>
+  <div class="px-1 carousel-card"><img src="img/SPO/SPO_b02.jpg" alt="" class="carousel-img rounded"></div>
+  <div class="px-1 carousel-card"><img src="img/SPO/SPO_b03.jpg" alt="" class="carousel-img rounded"></div>
+  <div class="px-1 carousel-card"><img src="img/SPO/SPO_b04.jpg" alt="" class="carousel-img rounded"></div>
+  <div class="px-1 carousel-card"><img src="img/FIG/FIG_b01.jpg" alt="" class="carousel-img rounded"></div>
+  <div class="px-1 carousel-card"><img src="img/FIG/FIG_b02.jpg" alt="" class="carousel-img rounded"></div>
+  <div class="px-1 carousel-card"><img src="img/FIG/FIG_b03.jpg" alt="" class="carousel-img rounded"></div>
 </div>
+
+
+<img class="position-absolute mt-3 me-3 end-0 all-game" src="img/all_game.png" alt="すべて">
+
+<div class="ms-5 mt-4">    
+  <h4>最新作</h4>
+  <div class="game_list ml-4">
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s02.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s03.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s04.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+    <div class="card border-0 px-1">
+            <img src="img/ACT/ACT_s01.jpg" class="card-img-top rounded">
+            <div class="card-text">ゲーム名</div>
+            <h5 class="card-title">1000円</h5>
+    </div>
+  </div>
+</div>
+
 
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 <script type="text/javascript" src="js/carousel.js"></script>
+
 <?php include_once 'GameFooter.php'; ?>
--- js/carousel.js
+++ js/carousel.js
@@ -1,10 +1,19 @@
 $(function(){
     $('.carousel').slick({
-      dots: true,
+    //   dots: true,
       infinite: true,
       speed: 300,
       slidesToShow: 1,
       centerMode: true,
       variableWidth: true
     });
+    $('.game_list').slick({
+      //   dots: true,
+        // infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 1,
+        centerMode: false,
+        variableWidth: true
+      });
   });
