740a62d4999280b70f22f97f8c06f2f7e0aaacb6
シンエイジュ <48903619+an9us@users.noreply.github.com>
2022-10-30 20:43:30 +0900

    NAVを綺麗にした

--- G1-1_Top.php
+++ G1-1_Top.php
@@ -2,8 +2,32 @@
 <?php include_once 'GameNavbar.php'; ?>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
 <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css"/>
-<link rel="stylesheet" href="css/top.css">
+<!-- <link rel="stylesheet" href="css/top.css"> -->
+<style>
+        .carousel-card{
+        height: auto;
+        }
 
+        .carousel-img{
+        height: 380px;
+        }
+        .card{
+        width: 18rem;
+        height: auto;
+        }
+
+        .all-game{
+        height: 30px;
+        }
+
+        .carousel{
+        visibility: hidden;
+        }
+
+        .carousel.slick-initialized{
+        visibility: visible;
+        }
+</style>
 
 <div class="carousel mt-3">
   <div class="px-1 carousel-card"><img src="img/SPO/SPO_b01.jpg" alt="" class="carousel-img rounded"></div>
@@ -15,8 +39,8 @@
   <div class="px-1 carousel-card"><img src="img/FIG/FIG_b03.jpg" alt="" class="carousel-img rounded"></div>
 </div>
 
-
-<img class="position-absolute mt-3 me-3 end-0 all-game" src="img/all_game.png" alt="すべて">
+<!-- 右のすべて -->
+<img class="position-absolute me-3 end-0 all-game" src="img/all_game.png" alt="すべて">
 
 <div class="ms-5 mt-4">    
   <h4>最新作</h4>
@@ -134,6 +158,31 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-<script type="text/javascript" src="js/carousel.js"></script>
+<!-- <script type="text/javascript" src="js/carousel.js"></script> -->
+<script>
+        $(function(){
+        $('.carousel').slick({
+                adaptiveHeight:true,
+                dots: true,
+                infinite: true,
+                speed: 300,
+                slidesToShow: 1,
+                centerMode: true,
+                variableWidth: true
+        });
+        $('.game_list').slick({
+        //   dots: true,
+                // infinite: true,
+                // adaptiveHeight:true,
+                // arrows: true,
+                // speed: 300,
+                rows: 1,
+                slidesPerRow: 20,
+                slidesToShow: 1,
+                // centerMode: false,
+                variableWidth: true
+        });
+        });
+</script>
 
 <?php include_once 'GameFooter.php'; ?>
--- GameNavbar.php
+++ GameNavbar.php
@@ -1,15 +1,47 @@
-<link rel="stylesheet" href="css/nav.css">
+<!-- <link rel="stylesheet" href="css/nav.css"> -->
+<style>
+@import url("https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.0/css/ionicons.min.css");
+@import url("https://cdnjs.cloudflare.com/ajax/libs/typicons/2.1.2/typicons.min.css");
+@import url("https://fonts.googleapis.com/earlyaccess/nicomoji.css");
+@import url("https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c");
+
+body { font-family: "M PLUS Rounded 1c"; }
+.wf-nicomoji { font-family: "Nico Moji"; }
+
+.search {
+  height: 30px;
+  background: white;
+  border-radius: 1rem;
+  border: none;
+  /* border-bottom: 1px solid black; */
+  /* outline:none;
+  box-shadow:none; */
+}
+
+/* Navbar背景の設定 */
+
+.navbar-bg {
+  background-color: gainsboro;
+  height:50px;
+}
+/* icon */
+i{
+  color: #000000;
+}
+
+
+</style>
 <!-- NavBar -->
 
-<nav class="navbar navbar-light navbar-expand-md py-3">
+<nav class="navbar navbar-light navbar-expand-md py-3 navbar-bg">
       <div class="container">
         <!-- 三みたいなメニューボタン -->
-        <span class=" d-flex justify-content-center align-items-center me-3 bs-icon">
-          <i class="typcn typcn-th-menu"></i>
+        <span class=" d-flex justify-content-center align-items-center me-3 mt-2">
+          <h5><i class="typcn typcn-th-menu"></i></h5>
         </span>
         <!-- LOGO文字 -->
-        <a class="navbar-brand d-flex align-items-center" href="#">
-          <span class="wf-nicomoji">ゲームECサイト</span>
+        <a class="navbar-brand d-flex align-items-center mt-1" href="#">
+          <h3 class="wf-nicomoji">ゲームECサイト</h3>
         </a>
         
         <!-- <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navcol-2">
@@ -19,22 +51,23 @@
         <div id="navcol-2" class="collapse navbar-collapse">
           <ul class="navbar-nav ms-auto"> <!-- ms-auto右寄せ -->
             
-            <li class="nav-item"><!-- 検索 -->
+            <li class="nav-item mt-3 me-3"><!-- 検索 -->
               <form class="form-inline" role="search" action="/Search/SearchList" method="get" target="_blank">
-                <div class="input-group mb-3">
+                <div class="input-group">
                   <input type="search" class="form-control search" placeholder="ゲームを探す">
                   <span class="input-group-text search"><i class="icon ion-search"></i></span>
                 </div>
              </form>
             </li>
-
-            <li class="nav-item"><!-- ハートマークicon -->
-              <a class="nav-link active" href="#">
-                <i class="typcn typcn-heart-outline"></i></a>
+            
+          
+        
+            <li class="nav-item d-flex justify-content-center align-items-center"><!-- ハートマークicon -->
+                <a class="nav-link mt-2" href="#"><h5><i class="typcn typcn-heart-outline"></i></h5></a>
             </li>
 
-            <li class="nav-item"><!-- カートicon -->
-              <a class="nav-link active" href="#"><i class="typcn typcn-shopping-cart"></i></a>
+            <li class="nav-item  d-flex justify-content-center align-items-center"><!-- カートicon -->
+              <a class="nav-link mt-2" href="#"><h5><i class="typcn typcn-shopping-cart"></i><h5></a>
             </li>
           </ul>
         </div>
