b571c515501dbd508075ac8c6b99a308ab1d906b
tutumiharuka <2101399@s.asojuku.ac.jp>
2022-10-31 11:11:18 +0900

    G1-5-2商品詳細更新しました。
    G1-5-2商品詳細更新しました。
    ・画像
    ・タイトル

--- G1-5-2_ShohinDetails.php
+++ G1-5-2_ShohinDetails.php
@@ -3,8 +3,19 @@
 
 
 <!-- ここで　PHP　と　BootStarp　を直接書きます -->
+<!-- 画像 -->
+<div class="container">
+    <div class="row">
+        <img class="col-md-8 mt-3 mx-auto" src="img/ACT/ACT_b01.jpg">
+    </div>
 
-<img src="img/ACT_b01.jpg" alt="" >
+
+<!-- 商品名 -->
+    <div class="row">
+        <div class="ms-5 mt-3">
+        <h2>スプラトューン３</h2>
+    </div
+
 
 
 <?php include_once 'GameFooter.php'; ?>
