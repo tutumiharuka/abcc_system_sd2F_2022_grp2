7558d58b5d00da064abceaed07072858fb74dad9
tutumiharuka <2101399@s.asojuku.ac.jp>
2022-11-01 11:34:32 +0900

    G1-5-2更新しました。
    大雑把に作成しました。
    細かいところはまだ何もしていません。
    これからする予定です。

--- G1-5-2_ShohinDetails.php
+++ G1-5-2_ShohinDetails.php
@@ -10,6 +10,10 @@
     .font {
         font-size: 30px;
     }
+
+    .fontD {
+        font-size: 20px
+    }
 </style>
 
 <!-- ここで　PHP　と　BootStarp　を直接書きます -->
@@ -35,7 +39,55 @@
         <div class="col-md-3 mt-4 font">
             <input type="submit" class="btn-light rounded-pill" value="カートに入れる">
         </div>
+<!-- 商品説明 -->
+        <div class="fontD">
+            <p>ヒトの姿に変身する不思議なイカたちによる、アクションシューティングがパワーアップして登場<br>
+               4対4のチームに分かれて、地面を塗った面積で勝敗を決める基本的なルールはそのままに、新たなブキやスペシャルウェポン、バトルアクションが追加。<br>
+               オンラインでフレンドや見知らぬ人と対戦できることはもちろん、本体を持ち寄って仲間と顔を合わせての対戦も可能。よりダイナミックになったナワバリバトルで、存分にインクを塗りたくれ!</p>
+        </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-3 m-7 fontD">
+            <p>必要な容量:</p>
+        </div>
+        <div class="col-md-3 font">
+            <p><strong>5.2G</strong></p>
+        </div>
+    </div>  
+    <div class="row">
+        <div class="col-md-3 m-7 fontD">
+            <p>対応ハード:</p>
+        </div>
+        <div class="col-md-3 font">
+            <p><strong>Nintendo switch</strong></p>
+        </div>
+    </div> 
+    <div class="row">
+        <div class="col-md-3 m-7 fontD">
+            <p>メーカー:</p>
+        </div>
+        <div class="col-md-3 font">
+            <p><strong>任天堂</strong></p>
+        </div>
+    </div> 
+    <div class="row">
+        <div class="col-md-3 m-7 fontD">
+            <p>配信日:</p>
+        </div>
+        <div class="col-md-3 font">
+            <p><strong>2022年9月9日</strong></p>
+        </div>
+    </div>  
+
+    <!-- ナビバーの設定かな -->
+    <nav class="mt-3 ms-3" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item h5"><a href="#">Top</a></li>
+            <li class="breadcrumb-item h5"><a href="#">アクション</a></li>
+            <li class="breadcrumb-item h5 active" aria-current="page">スプラトゥーン3</li>
+        </ol>
+    </nav>
 
 
 
