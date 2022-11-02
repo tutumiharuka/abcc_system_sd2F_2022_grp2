a5c9a52bedbc9f80132a34751bde239dfcdedd80
Nanopippi <116529074+Nanopippi@users.noreply.github.com>
2022-11-02 12:27:32 +0900

    Update G1-2-1_Login.php

--- G1-2-1_Login.php
+++ G1-2-1_Login.php
@@ -8,18 +8,22 @@
 <h1>ログイン</h1><br>
 </div>
 <div class="row m-1 mt-4">
-                                <div class="col-md-6 center">
+                                <div class="col-md-6">
                           <label  for="lastname"></label>
                               <input  type="text"  class="form-control"  id="lastname" placeholder="メールアドレス">
 			     
 </div>
 </div>
 <div class="row m-1 mt-4">
-<div class="col-md-6 center">
+<div class="col-md-6">
                        <label  for="lastname"></label>
                                     <input  type="text"  class="form-control"  id="lastname" placeholder="パスワード">			
                             
                                       </div>
+</div>
+<div class="d-grid gap-2">
+                                                    <button class="btn btn-info" type="button"><div class="container text-black">ログイン</div></button>
+                                                  </div>
 </div>
 </div>
 
