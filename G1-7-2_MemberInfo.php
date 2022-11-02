9cf85e1905034f291e716f1c7b2c1810562b591a
ikedayou <2101374@s.asojuku.ac.jp>
2022-11-02 14:43:40 +0900

    1-7-2

--- G1-7-2_MemberInfo.php
+++ G1-7-2_MemberInfo.php
@@ -11,6 +11,7 @@
     <!-- カード -->
     <div class="card">
       <div class="card-header">
+        <div class="container-fluid alert-danger">
         <h2>プロフィール<h2>
       </div>
 <!-- カードの内容 -->
@@ -25,37 +26,38 @@
           </li>
 
           <li class="list-group-item">
-            <div>
+            <div class="text-center">
               <label>生年月日</label>
               <input type="text" >
             </div>
           </li>
 
           <li class="list-group-item">
-            <div>
+            <div class="text-center">
               <label>メールアドレス</label>
               <input type="text">
             </div>
           </li>
 
           <li class="list-group-item">
-            <div>
+            <div class="text-center">
               <label>パスワード</label>
               <input type="text">
             </div>
           </li>
           
           <li class="list-group-item"><div>
+          <div class="text-center">
           <label>電話番号</label>
           <input type="text">
         </div></li>
       </ul>
+      <div class="text-center">
       <button type="submit">変更する</button>
     </div>
   </div>
-   
-
-    
+</div>
+</div>
 </div>
 
 <?php include_once 'GameFooter.php'; ?>
