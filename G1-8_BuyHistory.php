67432b080517a8950709594bf1ed0aa4987e4770
locks7 <Locks7123@gmail.com>
2022-11-02 11:38:20 +0800

    historyを更新しました

--- G1-8_BuyHistory.php
+++ G1-8_BuyHistory.php
@@ -1,8 +1,100 @@
 <?php include_once 'GameHeader.php'; ?>
 <?php include_once 'GameNavbar.php'; ?>
 
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>購入履歴</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css"
+        integrity="sha384-xeJqLiuOvjUBq3iGOjvSQSIlwrpqjSHXpduPd6rQpuiM3f5/ijby8pCsnbu5S81n" crossorigin="anonymous">
+    <style>
+
+    </style>
+</head>
+
 
 <!-- ここで　PHP　と　BootStarp　を直接書きます -->
+<!-- 購入履歴 -->
 
+<body>
+    <!-- t -->
+    <div class="container-fluid">
+        <div class="row ms-5 mt-5">
+            <p class="fs-1">購入履歴</p>
+        </div>
+
+        <!-- ｓ -->
+        <div class="row mt-5 ms-5">
+            <div class="col"><img src="img/FPS/FPS_s01.jpg" class="rounded float-start" alt="FPS_s01"></div>
+            <div class="col"><u><h4>ゲーム名</h4></u><br>
+                <p class="text-sm-start">購入日:2022年11月1日</p><br>
+                <i class="bi bi-box-arrow-in-right"></i>
+            </div>
+            <div class="col"><img src="img/FPS/FPS_s02.jpg" class="rounded float-start" alt="FPS_s02"></div>
+            <div class="col"><u><h4>ゲーム名</h4></u><br>
+                <p class="text-sm-start">購入日:2022年10月15日</p><br>
+                <i class="bi bi-box-arrow-in-right"></i>
+            </div>
+        </div>
+
+        <div class="row mt-5 ms-5">
+            <div class="col"><img src="img/FPS/FPS_s01.jpg" class="rounded float-start" alt="FPS_s01"></div>
+            <div class="col"><u>
+                    <h4>ゲーム名</h4>
+                </u><br>
+                <p class="text-sm-start">購入日:2022年11月1日</p><br>
+                <i class="bi bi-box-arrow-in-right"></i>
+            </div>
+            <div class="col"><img src="img/FPS/FPS_s02.jpg" class="rounded float-start" alt="FPS_s02"></div>
+            <div class="col"><u>
+                    <h4>ゲーム名</h4>
+                </u><br>
+                <p class="text-sm-start">購入日:2022年10月15日</p><br>
+                <i class="bi bi-box-arrow-in-right"></i>
+            </div>
+        </div>
+        
+        <div class="row mt-5 ms-5">
+            <div class="col"><img src="img/FPS/FPS_s01.jpg" class="rounded float-start" alt="FPS_s01"></div>
+            <div class="col"><u>
+                    <h4>ゲーム名</h4>
+                </u><br>
+                <p class="text-sm-start">購入日:2022年11月1日</p><br>
+                <i class="bi bi-box-arrow-in-right"></i>
+            </div>
+            <div class="col"><img src="img/FPS/FPS_s02.jpg" class="rounded float-start" alt="FPS_s02"></div>
+            <div class="col"><u>
+                    <h4>ゲーム名</h4>
+                </u><br>
+                <p class="text-sm-start">購入日:2022年10月15日</p><br>
+                <i class="bi bi-box-arrow-in-right"></i>
+            </div>
+        </div>
+        
+        <div class="row mt-5 ms-5">
+            <div class="col"><img src="img/FPS/FPS_s01.jpg" class="rounded float-start" alt="FPS_s01"></div>
+            <div class="col"><u>
+                    <h4>ゲーム名</h4>
+                </u><br>
+                <p class="text-sm-start">購入日:2022年11月1日</p><br>
+                <i class="bi bi-box-arrow-in-right"></i>
+            </div>
+            <div class="col"><img src="img/FPS/FPS_s02.jpg" class="rounded float-start" alt="FPS_s02"></div>
+            <div class="col"><u>
+                    <h4>ゲーム名</h4>
+                </u><br>
+                <p class="text-sm-start">購入日:2022年10月15日</p><br>
+                <i class="bi bi-box-arrow-in-right"></i>
+            </div>
+        </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+        crossorigin="anonymous"></script>
+</body>
 
 <?php include_once 'GameFooter.php'; ?>
