f8fbf3f8a405f021db417e8d4fc3b2cafaff685c
シン　エイジュ <48903619+an9us@users.noreply.github.com>
2022-11-02 12:53:58 +0900

    Update G1-8_BuyHistory.php

--- G1-8_BuyHistory.php
+++ G1-8_BuyHistory.php
@@ -17,11 +17,65 @@
     </style>
 </head>
 
-
-<!-- ここで　PHP　と　BootStarp　を直接書きます -->
-<!-- 購入履歴 -->
-
 <body>
+
+<div class="container overflow-hidden">
+  <div class="row gx-5">
+    <div class="col">
+     <div class="p-3 border bg-light">
+           
+
+                    <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                        <img src="..." alt="...">
+                        </div>
+                        <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+
+
+
+
+     </div>
+    </div>
+    <div class="col">
+      <div class="p-3 border bg-light">
+        
+
+
+                 <div class="card mb-3" style="max-width: 540px;">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                        <img src="..." alt="...">
+                        </div>
+                        <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+
+
+
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+    <!-- 購入履歴 -->
     <!-- t -->
     <div class="container-fluid">
         <div class="row ms-5 mt-5">
