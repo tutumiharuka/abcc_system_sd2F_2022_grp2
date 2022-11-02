30848c309bc0f5bdd841ae0dea3599acf4331ea6
tutumiharuka <2101399@s.asojuku.ac.jp>
2022-11-03 02:49:16 +0900

    G1-2-2更新しました。
    一応完成しました。
    あとは確認して修正お願いします。

--- G1-2-2_LoginEnd.php
+++ G1-2-2_LoginEnd.php
@@ -1,7 +1,10 @@
 <?php include_once 'GameHeader.php'; ?>
 <?php include_once 'GameNavbar.php'; ?>
+
 <!-- ここで　PHP　と　BootStarp　を直接書きます -->
+
+<div class="text-center mt-5">
 <h1>ログインしました</h1><br>
-    <button a href="G1-1_Top.html">TOPへ</a></button>
+    <button a href="G1-1_Top.php">TOPへ</a></button>
 
 <?php include_once 'GameFooter.php'; ?>
