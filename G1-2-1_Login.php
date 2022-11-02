4b0b5dfa0bba675ae70a7bf69f573ac740abae4d
tutumiharuka <2101399@s.asojuku.ac.jp>
2022-11-03 02:43:57 +0900

    G1-2-1更新しました。
    一応完成しました。
    あとは確認して修正をお願いします。

--- G1-2-1_Login.php
+++ G1-2-1_Login.php
@@ -1,26 +1,46 @@
 <?php include_once 'GameHeader.php'; ?>
 <?php include_once 'GameNavbar.php'; ?>
 
+<style>
+    .right-aligned{
+        margin-right: 0;
+        margin-left: auto;
+        width: 30%;
+    }
+</style>
 
 <!-- ここで　PHP　と　BootStarp　を直接書きます -->
 <div class="container">
-    <div class="col-md-12 text-center mt-3 ">
+    <div class="text-center m-5">
         <h1>ログイン</h1>
     </div>
-    <!-- メールアドレス -->
-    <div class="row m-1 mt-4">
-         <div class="col-md-6 center">
-            <label  for="lastname"></label>
-            <input  type="text"  class="form-control"  id="lastname" placeholder="メールアドレス">
-		</div>
-    </div>
-    <!-- パスワード -->
-    <div class="row m-1 mt-4">
-        <div class="col-md-6 center">
-            <label  for="lastname"></label>
-            <input  type="text"  class="form-control"  id="lastname" placeholder="パスワード">			
+
+    <div class="row justify-content-center">
+        <div class="col-6">
+            <form>
+        <!-- メールアドレス -->
+                <div class="mb-5 ">
+                    <input type="email" class="form-control" id="mail1adress" placeholder="メールアドレス">
+                </div>
+        <!-- パスワード -->
+                <div class="mb-5">
+                    <input type="password" class="form-control" id="password" placeholder="パスワード">
+                </div>
+        <!-- ボタン -->
+                <div class="right-aligned">
+                    <button type="submit" class="btn btn-light btn-lg rounded-pill">ログイン</button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
+
+<div class="ms-3 me-3">
+    <hr>
+</div>
+<div class="text-center">
+    <p class="mb-3">アカウントをお持ちでない方<p>
+    <button type="submit" class="btn btn-light btn-lg rounded-pill">新規登録</button>
 </div>
 
 <?php include_once 'GameFooter.php'; ?>
