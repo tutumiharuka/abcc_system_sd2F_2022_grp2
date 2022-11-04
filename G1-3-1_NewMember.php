b077c6b711903fc8f40d5ddca3b98030b911b89d
シンエイジュ <48903619+an9us@users.noreply.github.com>
2022-11-05 01:39:51 +0900

    直した！

--- G1-3-1_NewMember.php
+++ G1-3-1_NewMember.php
@@ -1,94 +1,96 @@
 <?php include_once 'GameHeader.php'; ?>
 <?php include_once 'GameNavbar.php'; ?>
 
-
+<style>
+    .kuhaku{
+        height: 80px;
+    }
+</style>
 
 <div class="container">
-    <div class="text-center mt-3">
+
+    <div class="row kuhaku"></div>
+    <div class="row text-center mt-3">
         <h2>アカウントの作成</h2>
     </div>
 <!-- 新規登録のフォーム -->
     <div class="card bg-light mt-3">
         <div class="card-body">
-            <form>
-<!-- 名前 -->
-            <div class="row g-3 align-items-center">
-                <div class="col-auto"><label for="name" class="col-form-label">◆名前</label></div>
-                <div class="col-auto"><input type="text" id="newname" class="form-control rounded-pill" aria-describedby="Newname" placeholder="10文字以内"></div>
-            </div>
-<!-- メールアドレス -->
-            <div class="row g-3 align-items-center">
-                <div class="col-auto"><label for="name" class="col-form-label">◆メールアドレス</label></div>
-                <div class="col-auto"><input type="text" id="newname" class="form-control rounded-pill" aria-describedby="Newname" placeholder="xxxxxxx@xxx.xx.xx"></div>
-                <p>・パソコンやWEBメールアドレスをおすすめします</p>
-            </div>
-<!-- 電話番号 -->
-            <div class="row g-3 align-items-center">
-                <div class="col-auto"><label for="name" class="col-form-label">◆電話番号</label></div>
-                <div class="col-auto"><input type="text" id="newname" class="form-control rounded-pill" aria-describedby="Newname" placeholder="携帯電話"></div>
-            </div>
-<!-- 生年月日 -->
-            <div>
-                <div class="row"><label for="name" class="col-form-label">◆生年月日</label></div>
-                    <div class="">
-                        <select class="form-select">
-                            <div class="col-md-3">
-                                <option selected>西暦</option>
-                                <!-- php -->
-                                <?php
-                                    for ($i = 1950; $i <= 2022; $i++) {
-                                        echo '<option value="'.$i.'">'.$i.'</option>';
-                                    }
-                                ?>
-                            </div>
-                        </select>
-                        <!-- 月 -->
-                        <select class="form-select">
-                            <div class="col-md-3">
-                                <option selected>月</option>
-                                <!-- php -->
-                                <?php
-                                    for ($i = 1; $i <= 12; $i++) {
-                                        echo '<option value="'.$i.'">'.$i.'</option>';
-                                    }
-                                ?>
-
-                            </div>
-                        </select>
-                        <!-- 日 -->
-                        <select class="form-select">
-                            <div class="col-md-3">
-                                <option selected>日</option>
-                                <!-- php -->
-                                <?php
-                                    for ($i = 1; $i <= 31; $i++) {
-                                        echo '<option value="'.$i.'">'.$i.'</option>';
-                                    }
-                                ?>
-                            </div>
-                        </select>
+            <form  action="G1-3-2_NewMemberCheck.php" method="post">
+    <!-- 名前 -->
+                <div class="row mt-3">
+                    <div class="col-md-2 offset-md-2">◆名前</div>
+                    <div class="col-md-7"><input type="text" name="name" class="form-control rounded-pill" aria-describedby="Newname" placeholder="10文字以内"></div>
+                </div>
+    <!-- メールアドレス -->
+                <div class="row mt-3">
+                    <div class="col-md-2 offset-md-2">◆メールアドレス</div>
+                    <div class="col-md-7"><input type="text" name="mail" class="form-control rounded-pill" aria-describedby="Newname" placeholder="xxxxxxx@xxx.xx.xx"></div>
+                </div>
+                <div class="row">
+                    <div class="col-md-2 offset-md-2"></div>
+                    <div class="col-md-7">・パソコンやWEBメールアドレスをおすすめします</div>
+                </div>
+    <!-- 電話番号 -->
+                <div class="row mt-3">
+                    <div class="col-md-2 offset-md-2">◆電話番号</div>
+                    <div class="col-md-7"><input type="text" name="phone" class="form-control rounded-pill" aria-describedby="Newname" placeholder="携帯電話"></div>
+                </div>
+    <!-- 生年月日 -->
+    
+    
+                <div class="row mt-3">
+                    <div class="col-md-2 offset-md-2">◆生年月日</div>
+        <!-- 年 -->
+                    <div class="col-md-2">
+                        <select class="form-select rounded-pill text-center" name="year">
+                        <option selected>選択する</option>
+                        <?php for ($i = 1950; $i <= 2022; $i++) echo '<option value="'.$i.'">'.$i.'</option>';?></select>
+                    </div>
+                    <div class="col-md-1 d-flex justify-content-start align-items-center">
+                        年
+                    </div>
+        <!-- 月 -->
+                    <div class="col-md-1">
+                        <select class="form-select rounded-pill text-center" name="month">
+                        <option selected>選択する</option>
+                        <?php for ($i = 1; $i <= 12; $i++) echo '<option value="'.$i.'">'.$i.'</option>';?></select>
+                    </div>
+                    <div class="col-md-1 d-flex justify-content-start align-items-center">
+                        月
+                    </div>
+        <!-- 日 -->
+                    <div class="col-md-1">
+                        <select class="form-select rounded-pill text-center" name="day">
+                        <option selected>選択する</option>
+                        <?php for ($i = 1; $i <= 31; $i++) echo '<option value="'.$i.'">'.$i.'</option>';?></select>
+                    </div>
+                    <div class="col-md-1 d-flex justify-content-start align-items-center">
+                        日
                     </div>
                 </div>
-            </div>
+<!-- 月と日が転送されないよう、メモする -->
+    <!-- パスワード -->
+                <div class="row mt-3">
+                    <div class="col-md-2 offset-md-2">◆パスワード</div>
+                    <div class="col-md-7"><input type="text" class="form-control rounded-pill" aria-describedby="Newname" placeholder="半角英数混合8文字以上"></div>
+                </div>
+    <!-- パスワード再入力 -->
+                <div class="row mt-3">
+                    <div class="col-md-2 offset-md-2">◆パスワード再入力</div>
+                    <div class="col-md-7"><input type="text" name="pass" class="form-control rounded-pill" aria-describedby="Newname" placeholder="半角英数混合8文字以上"></div>
+                </div>
 
+                <div class="row mt-3">
+                    <div class="col d-flex justify-content-center">
+                        <button type="submit" class="btn btn-outline-primary btn-lg">登録する</button>
+                    </div>
+                </div>
 
-<!-- パスワード -->
-            <div class="row g-3 align-items-center">
-                <div class="col-auto"><label for="name" class="col-form-label">◆パスワード</label></div>
-                <div class="col-auto"><input type="text" id="newname" class="form-control rounded-pill" aria-describedby="Newname" placeholder="半角英数混合8文字以上"></div>
-            </div>
-<!-- パスワード再入力 -->
-            <div class="row g-3 align-items-center">
-                <div class="col-auto"><label for="name" class="col-form-label">◆パスワード再入力</label></div>
-                <div class="col-auto"><input type="text" id="newname" class="form-control rounded-pill" aria-describedby="Newname" placeholder="半角英数混合8文字以上"></div>
-            </div>
-
-            </div>
-            
+                
             </form>
-        </div>
-    </div>
-
+        </div><!-- card-body -->
+    </div><!-- card -->
 </div>
 
 <?php include_once 'GameFooter.php'; ?>
--- G1-3-2_NewMemberCheck.php
+++ G1-3-2_NewMemberCheck.php
@@ -1,8 +1,22 @@
 <?php include_once 'GameHeader.php'; ?>
 <?php include_once 'GameNavbar.php'; ?>
 
+<style>
+    .kuhaku{
+        height: 80px;
+    }
+</style>
 
-<!-- ここで　PHP　と　BootStarp　を直接書きます -->
+<?php
+    $name = $_POST['name'];
+    $mail = $_POST['mail'];
+    $phone = $_POST['phone'];
+    $year = $_POST['year'];
+    $month = $_POST['month'];
+    $day = $_POST['day'];
+    $pass = $_POST['pass'];
 
+    echo $name.$mail.$phone.$year.$pass;
+?>
 
 <?php include_once 'GameFooter.php'; ?>
