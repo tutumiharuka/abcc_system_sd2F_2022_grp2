a83717d71f476e093781a8483efe1312677457a8
tutumiharuka <2101399@s.asojuku.ac.jp>
2022-11-04 16:11:35 +0900

    G1-6-3更新しました。
    確認お願いします。

--- G1-6-3_BuyEnd.php
+++ G1-6-3_BuyEnd.php
@@ -1,11 +1,30 @@
 <?php include_once 'GameHeader.php'; ?>
 <?php include_once 'GameNavbar.php'; ?>
 
-
+<style>
+    .kuhaku{
+        height: 100px;
+    }
+    .moji{
+        height: 150px;
+    }
+    .botan{
+        width: 200px;
+        height: 50px;
+    }
+</style>
 <!-- ここで　PHP　と　BootStarp　を直接書きます -->
-<h1>情報変更完了しました</h1><br>
-<h3>続けて手続きを行います</h3><br>
-<h5>お買い上げ内容が書かれたメールを、アソウアカウントのメールアドレスにお送りします。
-    子どもアカウントの場合は保護者（ファミリーの管理者）のメールアドレスにお送りします。</h5>
-    <button a href="G1-6-4_Send.php">転送</a></button>
+
+<div class="container">
+    <div class="row kuhaku"></div>
+    <div class="row text-center moji"><h1>情報変更完了しました</h1></div>
+    <div class="row text-center moji"><h1>続けて手続きを行います</h1></div>
+    <div class="row text-center moji"><h3>お買い上げ内容が書かれたメールを、アソウアカウントのメールアドレスにお送りします。<br>子どもアカウントの場合は保護者（ファミリーの管理者）のメールアドレスにお送りします。</h3></div>
+    <div class="row ">
+        <div class="col d-flex justify-content-center">
+            <a class="btn btn-primary botan" href="G1-6-4_Send.php"><h2>転送</h2></a>
+        </div>
+    </div>
+</div>
+
 <?php include_once 'GameFooter.php'; ?>
