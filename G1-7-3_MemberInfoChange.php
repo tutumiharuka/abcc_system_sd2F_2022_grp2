78a2d002373cdd163e37fca819c8da1ee7879396
ikedayou <2101374@s.asojuku.ac.jp>
2022-11-04 10:12:13 +0900

    G1-7-3

--- G1-7-3_MemberInfoChange.php
+++ G1-7-3_MemberInfoChange.php
@@ -3,37 +3,60 @@
 
 
 <!-- ここで　PHP　と　BootStarp　を直接書きます -->
-<div>
-  <div>
-    <div>
-      会員情報<br>
-      プロフィール
+<div class="container">
+    <div class="row text-center">
+      <h2>会員情報<h2>
     </div>
-    <form>
-      <div>
-        <div>
-          <label>おなまえ</label>
-          <input type="text">
-        </div>
-        <div>
-          <label>生年月日</label>
-          <input type="text" value="2022年12月31日">
-        </div>
-        <div>
-          <label>メールアドレス</label>
-          <input type="text">
-        </div>
-        <div>
-          <label>パスワード</label>
-          <input type="text">
-        </div>
-        <div>
+    <!-- カード -->
+    <div class="card">
+      <div class="card-header">
+        <div class="container-fluid alert-danger">
+        <h2>プロフィール<h2>
+      </div>
+<!-- カードの内容 -->
+      <div class="card-body bg-light">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">
+
+            <div class="text-center">
+              <label>おなまえ</label>
+              <input type="text">
+            </div>
+          </li>
+
+          <li class="list-group-item">
+            <div class="text-center">
+              <label>生年月日</label>
+              <input type="month" name="example" value="2022-05">
+            </div>
+          </li>
+
+          <li class="list-group-item">
+            <div class="text-center">
+              <label>メールアドレス</label>
+              <input type="text">
+            </div>
+          </li>
+
+          <li class="list-group-item">
+            <div class="text-center">
+              <label>パスワード</label>
+              <input type="text">
+            </div>
+          </li>
+          
+          <li class="list-group-item"><div>
+          <div class="text-center">
           <label>電話番号</label>
           <input type="text">
-        </div>
-        <button type="submit">変更する</button>
-      </div>
-    </form>
+        </div></li>
+      </ul>
+      <div class="text-center">
+      <button type="submit">変更する</button>
+    </div>
   </div>
 </div>
+</div>
+</div>
+
 
