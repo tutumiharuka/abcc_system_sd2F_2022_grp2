c832e7faf6388c65c3b7074d3b8230c5a1342973
tutumiharuka <2101399@s.asojuku.ac.jp>
2022-11-04 16:18:58 +0900

    G1-3-3更新しました。
    確認お願いします。

--- G1-3-3_NewMemberEnd.php
+++ G1-3-3_NewMemberEnd.php
@@ -1,9 +1,28 @@
 <?php include_once 'GameHeader.php'; ?>
 <?php include_once 'GameNavbar.php'; ?>
 
-
+<style>
+    .kuhaku{
+        height: 200px;
+    }
+    .moji{
+        height: 150px;
+    }
+    .botan{
+        width: 200px;
+        height: 50px;
+    }
+</style>
 <!-- ここで　PHP　と　BootStarp　を直接書きます -->
-<h1>登録しました</h1><br>
-<button a href="G1-1_Top.html">TOPへ</a></button>
+<div class="container">
+    <div class="row kuhaku"></div>
+    <div class="row text-center moji"><h1>登録しました</h1></div>
+    <div class="row ">
+        <div class="col d-flex justify-content-center">
+            <a class="btn btn-primary botan" href="G1-1_Top.php"><h2>TOPへ</h2></a>
+        </div>
+    </div>
+</div>
+
 
 <?php include_once 'GameFooter.php'; ?>
