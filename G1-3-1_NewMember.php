ac70c23d0defc2840efaa9b6135d2fbb94c0d5c7
シンエイジュ <48903619+an9us@users.noreply.github.com>
2022-11-05 22:37:27 +0900

    会員登録画面に、パスワードの確認処理

--- G1-3-1_NewMember.php
+++ G1-3-1_NewMember.php
@@ -7,8 +7,9 @@
     }
 </style>
 
-<div class="container">
 
+
+<div class="container">
     <div class="row kuhaku"></div>
     <div class="row text-center mt-3">
         <h2>アカウントの作成</h2>
@@ -16,16 +17,16 @@
 <!-- 新規登録のフォーム -->
     <div class="card bg-light mt-3">
         <div class="card-body">
-            <form  action="G1-3-2_NewMemberCheck.php" method="post">
+            <form id="form" action="#" onsubmit="return on_submit()">
     <!-- 名前 -->
                 <div class="row mt-3">
                     <div class="col-md-2 offset-md-2">◆名前</div>
-                    <div class="col-md-7"><input type="text" name="name" class="form-control rounded-pill" aria-describedby="Newname" placeholder="10文字以内"></div>
+                    <div class="col-md-7"><input type="text" name="name" class="form-control rounded-pill" placeholder="10文字以内" required></div>
                 </div>
     <!-- メールアドレス -->
                 <div class="row mt-3">
                     <div class="col-md-2 offset-md-2">◆メールアドレス</div>
-                    <div class="col-md-7"><input type="text" name="mail" class="form-control rounded-pill" aria-describedby="Newname" placeholder="メールを入力してください"></div>
+                    <div class="col-md-7"><input type="mail" name="mail" class="form-control rounded-pill" placeholder="メールを入力してください" required></div>
                 </div>
                 <div class="row">
                     <div class="col-md-2 offset-md-2"></div>
@@ -33,16 +34,17 @@
     <!-- 電話番号 -->
                 <div class="row mt-3">
                     <div class="col-md-2 offset-md-2">◆電話番号</div>
-                    <div class="col-md-7"><input type="text" name="phone" class="form-control rounded-pill" aria-describedby="Newname" placeholder="携帯電話"></div>
+                    <div class="col-md-7"><input type="tel" name="phone" class="form-control rounded-pill" placeholder="携帯電話を11桁で入力してください" pattern="[0-9]{11}" required></div>
                 </div>
     <!-- 生年月日 -->
     
     
                 <div class="row mt-3">
                     <div class="col-md-2 offset-md-2">◆生年月日</div>
+                    
         <!-- 年 $y -->
                     <div class="col-md-2">
-                        <select class="form-select rounded-pill text-center" name="year">
+                        <select class="form-select rounded-pill text-center" name="year" required>
                         <!-- <option selected>選択する</option> -->
                         <?php for ($y = 1950; $y <= date("Y"); $y++) echo '<option value="'.$y.'">'.$y.'</option>';?></select>
                     </div>
@@ -51,7 +53,7 @@
                     </div>
         <!-- 月 $m -->
                     <div class="col-md-1">
-                        <select class="form-select rounded-pill text-center" name="month">
+                        <select class="form-select rounded-pill text-center" name="month" required>
                         <!-- <option selected>選択する</option> -->
                         <?php for ($m = 1; $m <= 12; $m++) echo '<option value="'.$m.'">'.$m.'</option>';?></select>
                     </div>
@@ -60,7 +62,7 @@
                     </div>
         <!-- 日 $d -->
                     <div class="col-md-1">
-                        <select class="form-select rounded-pill text-center" name="day">
+                        <select class="form-select rounded-pill text-center" name="day" required>
                         <!-- <option selected>選択する</option> -->
                         <?php for ($d = 1; $d <= 31; $d++) echo '<option value="'.$d.'">'.$d.'</option>';?></select>
                     </div>
@@ -72,12 +74,17 @@
     <!-- パスワード -->
                 <div class="row mt-3">
                     <div class="col-md-2 offset-md-2">◆パスワード</div>
-                    <div class="col-md-7"><input type="password" class="form-control rounded-pill" aria-describedby="Newname" placeholder="半角英数混合8文字以上"></div>
+                    <div class="col-md-7"><input type="password" name="pass" id="pass" class="form-control rounded-pill" placeholder="半角英数混合8文字以上" required></div>
                 </div>
     <!-- パスワード再入力 -->
                 <div class="row mt-3">
                     <div class="col-md-2 offset-md-2">◆パスワード再入力</div>
-                    <div class="col-md-7"><input type="password" name="pass" class="form-control rounded-pill" aria-describedby="Newname" placeholder="半角英数混合8文字以上"></div>
+                    <div class="col-md-7"><input type="password" id="repass" class="form-control rounded-pill" placeholder="半角英数混合8文字以上" required></div>
+                </div>
+                
+                <div class="row">
+                    <div class="col-md-2 offset-md-2"></div>
+                    <div id="err" class="col-md-7 d-none text-danger">入力したパスワードが間違っていますので、確認してください。</div>
                 </div>
 
                 <div class="row mt-3">
@@ -85,11 +92,39 @@
                         <button type="submit" class="btn btn-outline-primary btn-lg">登録する</button>
                     </div>
                 </div>
-
                 
             </form>
         </div><!-- card-body -->
     </div><!-- card -->
 </div>
 
+<script>
+    // 入力したパスワードが半角英数混合8文字以上かという確認
+    function check(str){
+        var reg = new RegExp(/^(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{8,}$/); //上記の正規表現を引数に渡す
+        var response = reg.test(str);
+        return response;
+    }
+    //確認ボタンを押す瞬間に確認する動作です。パスワードの確認する。
+    //大丈夫だったら、次のページの値を与える
+	function on_submit(){
+        let form = document.getElementById("form");
+		let pass = document.getElementById("pass");
+		let repass = document.getElementById("repass");
+		let err = document.getElementById("err");
+
+        if(pass.value==repass.value){
+            form.action="G1-3-2_NewMemberCheck.php";
+            form.method="post";
+            frm.encoding="application/x-www-form-urlencoded";
+            err.classList.add("d-none");
+            return true;
+        }else if(pass.value!=repass.value || check(pass.value)!=true){
+            err.classList.remove("d-none");
+			return false;
+        }
+      
+        
+	}
+</script>
 <?php include_once 'GameFooter.php'; ?>
