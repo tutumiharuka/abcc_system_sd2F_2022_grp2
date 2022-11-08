dd42d26d7c6b4079bbd5d9cd648d780abe3f6e8e
シン　エイジュ <48903619+an9us@users.noreply.github.com>
2022-11-08 13:57:12 +0900

    転送中、動画を追加した

--- G1-6-4_Send.php
+++ G1-6-4_Send.php
@@ -1,74 +1,57 @@
 <?php include_once 'GameHeader.php'; ?>
-<style>
-	/* The animation code */
-	@keyframes load {
-	0%   {width: 0%;}
-	/* 25%  {background-color:yellow; left:200px; top:0px;}
-	50%  {background-color:blue; left:200px; top:200px;}
-	75%  {background-color:green; left:0px; top:200px;} */
-	100% {width: 100%;}
-	}
-
-	/* The element to apply the animation to */
-	.loading {
-		/* width: 100px; */
-		/* height: 100px; */
-		/* position: relative; */
-		/* background-color: red; */
-		animation-name: load;
-		animation-duration: 4s;
-		animation-iteration-count: 1;
-	}
-
-</style>
 <div class="row">
 	<div class="col-md-12 bg-info text-dark text-left">
-        <h1 class="p-3">転送が始まります...</h1>
+        <h1 class="p-3 ms-5" id="message">転送が始まります...</h1>
 	</div>
 </div>
-<div  class="text-center">
-	
-	<!-- <div class="progress">
-		<div class="progress-bar progress-bar-striped progress-bar-animated loading" ></div>
-	</div> -->
 
 
-<div class="row">
-	<div class="col m-4">
-		<!-- Progress bar HTML -->
-		<div class="progress">
-			<div class="progress-bar progress-bar-striped progress-bar-animated" style="min-width: 20px;"></div>
-		</div>	
-	</div>
-</div>
-	
-	
+<div  class="contianer text-center">	
 	<div class="img-center mt-5 rounded">
 		<img class="rounded" src="img/ACT/ACT_s01.jpg">
     </div>
-	<h2 class="m-4 text-center">スプラトゥーン３</h2>
 	<!-- ↑ここで商品画像を表示 -->
-	<a class="btn btn-primary mb-4 rounded text-dark rounded-pill" href="G1-1_Top.php"><h3>TOPに戻る</h3></a>
+	<h2 class="m-4 text-center">スプラトゥーン３</h2>
+	
+	<!-- 転送進度を示すProgress bar -->
+	<div class="row d-flex justify-content-center">
+		<div class="col-md-6 m-4">
+			<div class="progress">
+				<div class="progress-bar progress-bar-striped progress-bar-animated" style="min-width: 20px;" aria-valuemin="0" aria-valuemax="100">
+				</div>
+			</div>	
+		</div>
+	</div>
+
+	<a class="btn btn-primary btn-lg mt-5 mb-4 rounded text-dark rounded-pill" href="G1-1_Top.php">TOPに戻る</a>
 </div>
+
 
 
 <!-- JavaScript -->
 <script>
-        var i = 0;
-      	var bar = document.querySelector(".progress-bar");
-        function makeProgress(){
-            if(i < 100){
-                i++;
-              	bar.style.width = i + "%";
-              	// bar.innerText = i + "%";
-				
-            }
+	let i = 0;
+	let bar = document.querySelector(".progress-bar");
+	let message = document.getElementById("message");
+	function makeProgress(){
+		if(i < 100){
+			i++;
+			bar.style.width = i + "%";
+			// bar.innerText = i + "%";
+			if(i>=10){
+				message.textContent = "転送中..."
+			}
+			if(i>=99){
+				message.textContent = "転送完了"
+				bar.classList.remove("progress-bar-striped");
 
-            // Wait for sometime before running this script again
-            setTimeout("makeProgress()", 50);
-        }
-        makeProgress();
-    </script>
+			}
+			
+		}
+		setTimeout("makeProgress()", 50);
+	}
+	makeProgress();
+</script>
 
 
 
