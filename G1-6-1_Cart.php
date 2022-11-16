0d187f27d2470dc01bff26855878e6645dfcb700
シン　エイジュ <48903619+an9us@users.noreply.github.com>
2022-11-16 11:05:06 +0900

    カート画面なおした

--- G1-6-1_Cart.php
+++ G1-6-1_Cart.php
@@ -33,10 +33,11 @@ input[type="checkbox"]{
 <!-- カート -->
 
 <div class="container">
-    <div class="row ms-5 mt-5">
+    <div class="row mt-5">
         <ul class="list-inline">
-            <li class="list-inline-item h1">カート</li>
-            <li class="list-inline-item h5">(合計<?php echo $count ?>点)</li>
+            <li class="list-inline-item fw-bold h1">カート</li>
+            <li class="list-inline-item fw-bold h5">(合計<?php echo $count ?>点)</li>
+            
         </ul>
     </div>
     
@@ -44,56 +45,63 @@ input[type="checkbox"]{
 
     <div class="row">
         <!-- カートリスト -->
-        <div class="col-md-7">
-            <hr>
+        <div class="col-md-8">
+        
             <?php 
             foreach($results as $row){
-                echo '<div class="row mt-5">
-                            <div class="col-md-4"><img class="img-fluid" src="'.$row['image_small'].'"></div>
-                            <div class="col-md-6">
-                                <div class="row mt-5 h4">'.$row['shohin_name'].'</div>
-                                <div class="row text-sm-start mt-4 h5">税込:'.$row['price'].'円</div>
-                            </div>
-                            <div class="col-md-2">
-                                <form action="" method="post">
-                                    <input type="hidden" name="delcart" value="'.$row['cart_id'].'">
-                                    <button type="submit" class="btn btn-outline-light"><h4><i class="bi bi-x-square-fill"></i></h4></button>
-                                </form>
-                            </div>
-                    </div>';
+                echo '<hr>
+                <div class="row mt-5">
+                    <div class="col-md-4"><img class="img-fluid" src="'.$row['image_small'].'"></div>
+                    <div class="col-md-8">
+                        <div class="row text-end me-3">
+                            <form action="G1-6-1_Cart.php" method="post">
+                                <input type="hidden" name="delcart" value="'.$row['cart_id'].'">
+                                <button type="submit" class="btn btn-outline-light"><h4><i class="bi bi-x-square-fill"></i></h4></button>
+                            </form>
+                        </div>
+                        <div class="row ms-1 h4">'.$row['shohin_name'].'</div>
+                        <div class="row text-sm-start mt-5 ms-1 h5">税込:'.$row['price'].'円</div>
+                    </div>
+                </div>';
             }
             ?>
         </div>
         <!-- 合計 -->
-        <div class="col-md-5">
+        <div class="col-md-4">
             <div class="row">
-                <div class="col"><span class="h2">合計</span>(税込)</div>
-                <span class="col h3 text-end"><?php echo $sum?></span>
+                <div class="col fw-bold"><span class="h2 fw-bold">合計</span>(税込)</div>
+                <span class="col h3 fw-bold text-end "><?php echo $sum?>円</span>
             </div>
             <hr>
+            <div class="row m-1 gx-1">
+                <a class="btn btn-primary btn-lg rounded-pill" href="G1-6-2_BuyCheck.php">レジに進む</a>
+            </div>
+            <div class="row m-1 gx-1">
+                <a class="btn btn-outline-primary btn-lg rounded-pill" href="G1-1_Top.php">お買い物を続ける</a>
+            </div>
         </div>
     </div>
     
     <!-- 商品リスト -->
     <?php 
-     foreach($results as $row){
-        echo '<div class="row mt-5 ms-5">
-                    <div class="col-md-2 offset-md-1 d-flex justify-content-center align-items-center">
-                        <input class="form-check-input" type="checkbox">
-                    </div>
-                    <div class="col-md-3"><img class="img-fluid" src="'.$row['image_small'].'"></div>
-                    <div class="col-md-2">
-                        <div class="row mt-5 h4">'.$row['shohin_name'].'</div>
-                        <div class="row text-sm-start mt-4 h5">税込:'.$row['price'].'円</div>
-                    </div>
-                    <div class="col me-5">
-                    <form action="" method="post">
-                        <input type="hidden" name="delcart" value="'.$row['cart_id'].'">
-                        <button type="submit" class="btn btn-outline-light"><h4><i class="bi bi-x-square-fill"></i></h4></button>
-                    </form>
-                    </div>
-              </div>';
-     }
+    //  foreach($results as $row){
+    //     echo '<div class="row mt-5 ms-5">
+    //                 <div class="col-md-2 offset-md-1 d-flex justify-content-center align-items-center">
+    //                     <input class="form-check-input" type="checkbox">
+    //                 </div>
+    //                 <div class="col-md-3"><img class="img-fluid" src="'.$row['image_small'].'"></div>
+    //                 <div class="col-md-2">
+    //                     <div class="row mt-5 h4">'.$row['shohin_name'].'</div>
+    //                     <div class="row text-sm-start mt-4 h5">税込:'.$row['price'].'円</div>
+    //                 </div>
+    //                 <div class="col me-5">
+    //                 <form action="" method="post">
+    //                     <input type="hidden" name="delcart" value="'.$row['cart_id'].'">
+    //                     <button type="submit" class="btn btn-outline-light"><h4><i class="bi bi-x-square-fill"></i></h4></button>
+    //                 </form>
+    //                 </div>
+    //           </div>';
+    //  }
     ?>
    
 
