a1d682f7fcf358f19e30aa76234c4abf810b5a49
シンエイジュ <48903619+an9us@users.noreply.github.com>
2022-11-19 00:42:37 +0900

    購入履歴、転送ボタン以外完成

--- G1-8_BuyHistory.php
+++ G1-8_BuyHistory.php
@@ -3,6 +3,13 @@
     require_once 'LoginManager.php';
     $loginMng = new LoginManager(); 
     $loginMng->isLogin();
+
+    require_once "DBManager.php";
+    $dbmng = new DBManager();
+    $member_id = $_SESSION['member']['member_id'];
+    //注文したゲームをカートから削除し、履歴に追加する
+    $results = $dbmng->getBuyHistroyList($member_id);
+
 ?>
 <?php include_once 'GameHeader.php'; ?>
 <?php include_once 'GameNavbar.php'; ?>
@@ -11,41 +18,24 @@
     <!-- タイトル -->
     <div class="row h3">購入履歴</div>
     <!-- リストの一行 -->
-    <div class="row mt-5">
-        <!-- 一つゲーム -->
-        <div class="col-lg-3 col-md-6"><img class="img-fluid" src="img/FPS/FPS_s02.jpg"></div>
-        <div class="col-lg-3 col-md-6">
-            <div class="row h4 mt-5 ms-2 text-start">ゲームのタイトルです！！！！</div>
-            <div class="row h5 ms-2 mt-5">購入日:2022年11月1日</div>
-            <div class="row text-center me-5 mt-5"><i class="bi bi-box-arrow-in-right h2"></i></div>
-        </div>
-        
-        <div class="col-lg-3 col-md-6"><img class="img-fluid" src="img/FPS/FPS_s02.jpg"></div>
-        <div class="col-lg-3 col-md-6">
-            <div class="row h4 mt-5 ms-2 text-start">ゲームのタイトルです！！！！</div>
-            <div class="row h5 ms-2 mt-5">購入日:2022年11月1日</div>
-            <div class="row text-center me-5 mt-5"><i class="bi bi-box-arrow-in-right h2"></i></div>
-        </div>
+    <div class="row">
+        <?php
+        foreach($results as $row){
+            echo '  <div class="col-lg-3 mt-5">
+                        <a href="G1-5-2_ShohinDetails.php?shohin_id='.$row['shohin_id'].'">
+                        <img class="img-fluid" src="'.$row['image_small'].'"></a>
+                    </div>
+                    <div class="col-lg-3 mt-5">
+                        <a href="G1-5-2_ShohinDetails.php?shohin_id='.$row['shohin_id'].'">
+                        <div class="row h4 mt-5 text-start">'.$row['shohin_name'].'</div>
+                        <div class="row h5 ms-5 mt-5">購入日:'.$row['buying_date'].'</div></a>
+                        <div class="row text-center me-5 mt-5"><i class="bi bi-box-arrow-in-right h2"></i></div>
+                    </div>';
+        }
+        ?>
     </div>
 
-    <!-- リストの2行 -->
-    <div class="row mt-5">
-        <!-- 一つゲーム -->
-        <div class="col-lg-3 col-md-6"><img class="img-fluid" src="img/FPS/FPS_s02.jpg"></div>
-        <div class="col-lg-3 col-md-6">
-            <div class="row h4 mt-5 ms-2 text-starttitle">ゲームのタイトルです！！！！</div>
-            <div class="row h5 ms-2 mt-5">購入日:2022年11月1日</div>
-            <div class="row text-center me-5 mt-5"><i class="bi bi-box-arrow-in-right h2"></i></div>
-        </div>
-        
-        <div class="col-lg-3 col-md-6"><img class="img-fluid" src="img/FPS/FPS_s02.jpg"></div>
-        <div class="col-lg-3 col-md-6">
-            <div class="row h4 mt-5 ms-2 text-start">ゲームのタイトルです！！！！</div>
-            <div class="row h5 ms-2 mt-5">購入日:2022年11月1日</div>
-            <div class="row text-center me-5 mt-5"><i class="bi bi-box-arrow-in-right h2"></i></div>
-        </div>
-    </div>
-
+  
     
 </div>
 
