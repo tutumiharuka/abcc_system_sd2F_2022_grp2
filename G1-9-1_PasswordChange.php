90a9fde84794386c79059d88c31f89cd182af0c6
tutumiharuka <2101399@s.asojuku.ac.jp>
2022-11-25 14:45:43 +0900

    わかりやすくしただけ

--- G1-9-1_PasswordChange.php
+++ G1-9-1_PasswordChange.php
@@ -9,19 +9,15 @@
 </style>
 
 <div class="container">
-
-    <div class="row kuhaku-form"></div>
-
-    <div class="row text-center pb-5"><h2>パスワード変更<h2></div>
-
+  <div class="row kuhaku-form"></div>
+  <!-- タイトル -->
+  <div class="row text-center pb-5"><h2>パスワード変更<h2></div>
     <form action="G1-9-2_PasswordChangeCheck.php" method="post">
-
-          <div class="d-flex justify-content-center mt-2 mb-5">
-          
-            <div class="card ms-3" style="width: 60rem;">
-              <div class="card-header"><h2 class="ms-4 mt-2 fw-bold">プロフィール</h2></div>
+      <div class="d-flex justify-content-center mt-2 mb-5">
+  <!-- カード -->
+        <div class="card ms-3" style="width: 60rem;">
+           <div class="card-header"><h2 class="ms-4 mt-2 fw-bold">プロフィール</h2></div>
               <ul class="list-group list-group-flush">
-
                 <li class="list-group-item">
                   <div class="row h5">
                     <div class="col-md-3 offset-md-3 text-start">
@@ -35,6 +31,7 @@
             </div>
             
           </div>
+  <!-- 変更ボタン -->
           <div class="row mt-4">
             <div class="col d-flex justify-content-center">
               <button type="submit" class="btn btn-outline-primary btn-lg mb-3">変更する</button>
