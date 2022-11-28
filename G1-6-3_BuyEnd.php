b5a694f1b7ae5481df963f81b7643fb3a18a9bcc
シン　エイジュ <48903619+an9us@users.noreply.github.com>
2022-11-28 11:59:57 +0900

    PASSWORD EYE 途中

--- G1-6-3_BuyEnd.php
+++ G1-6-3_BuyEnd.php
@@ -32,16 +32,15 @@
         height: 50px;
     }
 </style>
-<!-- ここで　PHP　と　BootStarp　を直接書きます -->
 
 <div class="container">
     <div class="row kuhaku-end"></div>
     <!-- <div class="row text-center moji"><h1>情報変更完了しました</h1></div> -->
-    <div class="row text-center moji"><h1>続けて手続きを行います</h1></div>
-    <div class="row text-center moji"><h3>お買い上げ内容が書かれたメールを、アソウアカウントのメールアドレスにお送りします。<br>子どもアカウントの場合は保護者（ファミリーの管理者）のメールアドレスにお送りします。</h3></div>
+    <div class="row text-center moji"><h2>続けて手続きを行います</h2></div>
+    <div class="row text-center moji"><h4>お買い上げ内容が書かれたメールを、アソウアカウントのメールアドレスにお送りします。<br>子どもアカウントの場合は保護者（ファミリーの管理者）のメールアドレスにお送りします。</h4></div>
     <div class="row ">
         <div class="col d-flex justify-content-center">
-            <a class="btn btn-primary btn-lg rounded-pill" href="G1-6-4_Send.php"><h2>転送</h2></a>
+            <a class="btn btn-primary btn-lg rounded-pill botan fw-bold" href="G1-6-4_Send.php">転送</a>
         </div>
     </div>
 </div>
