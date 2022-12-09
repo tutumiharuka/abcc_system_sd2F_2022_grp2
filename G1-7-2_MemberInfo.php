f26cc7baff2b4eb268299385e6172a16e4541f36
シン　エイジュ <48903619+an9us@users.noreply.github.com>
2022-12-09 12:49:08 +0900

    Update G1-7-2_MemberInfo.php

--- G1-7-2_MemberInfo.php
+++ G1-7-2_MemberInfo.php
@@ -25,10 +25,14 @@
 <?php include_once 'GameNavbar.php'; ?>
 
 <style> 
-  .kuhaku-form{
-    height:15%;
-  }
-
+    .kuhaku-form{
+        height: 10%;
+    }
+    @media only screen and (max-width: 770px) {       
+        .kuhaku-from{
+            height: 5%;
+        }
+    }
 </style>
 <div class="container">
     <div class="row kuhaku-form">
@@ -50,41 +54,41 @@
 
           <li class="list-group-item">
             <div class="row h5">
-              <div class="col-md-3 offset-md-3 text-start">
+              <div class="col-6 col-md-3 offset-0 offset-md-3 text-start">
                 <i class="bi bi-diamond-fill h6 me-3"></i>名前</div>
-              <div class="col-md-6"><?php echo $name ?></div>
+              <div class="col-6 col-md-6"><?php echo $name ?></div>
             </div>
           </li>
 
           <li class="list-group-item">
             <div class="row h5">
-              <div class="col-md-3 offset-md-3 text-start">
+              <div class="col-6 col-md-3 offset-0 offset-md-3 text-start">
                 <i class="bi bi-diamond-fill h6 me-3"></i>メールアドレス</div>
-              <div class="col-md-6"><?php echo $mail ?></div>
+              <div class="col-6 col-md-6"><?php echo $mail ?></div>
             </div>
           </li>
 
           <li class="list-group-item">
             <div class="row h5">
-              <div class="col-md-3 offset-md-3 text-start">
+              <div class="col-6 col-md-3 offset-0 offset-md-3 text-start">
                 <i class="bi bi-diamond-fill h6 me-3"></i>電話番号</div>
-              <div class="col-md-6"><?php echo $phone ?></div>
+              <div class="col-6 col-md-6"><?php echo $phone ?></div>
             </div>
           </li>
       
           <li class="list-group-item">
             <div class="row h5">
-              <div class="col-md-3 offset-md-3 text-start">
+              <div class="col-6 col-md-3 offset-0 offset-md-3 text-start">
                 <i class="bi bi-diamond-fill h6 me-3"></i>生年月日</div>
-              <div class="col-md-6"><?php echo $date_of_birth ?></div>
+              <div class="col-6 col-md-6"><?php echo $date_of_birth ?></div>
             </div>
           </li>
 
           <li class="list-group-item">
             <div class="row h5">
-              <div class="col-md-3 offset-md-3 text-start">
+              <div class="col-6 col-md-3 offset-0 offset-md-3 text-start">
                 <i class="bi bi-diamond-fill h6 me-3"></i>パスワード</div>
-              <div class="col-md-6">●●●●●●●●●● 
+              <div class="col-6 col-md-6">●●●●●●●●●● 
                 <a class="btn btn-outline-primary ms-3" href="G1-9-1_PasswordChange.php">パスワード変更</a>
             </div>
             </div>
