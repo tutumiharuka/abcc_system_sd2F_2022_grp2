e45cc1c3071fd90aaec52bbbb0f7843244fa4d7e
ikedayou <2101374@s.asojuku.ac.jp>
2023-01-13 10:05:12 +0900

    rankingデータ

--- sql/04.newGame.sql
+++ sql/04.newGame.sql
@@ -3,26 +3,26 @@ INSERT INTO shohins VALUES (null,'BOK','ポケットモンスター スカーレ
 INSERT INTO shohins VALUES (null,'RPG','ドラゴンクエスト トレジャーズ　蒼き瞳と大空の羅針盤',7990,'2.9GB','2022-12-09','img/RPG/RPG_s21.jpg','img/RPG/RPG_b21.jpg','<h3>竜の大地で　気ままなお宝探し！</h3><br>バイキング船で暮らす仲良し兄妹のカミュとマヤは、ある日ふしぎな精霊たちに導かれて「はてしなき竜の大地」と呼ばれる異世界に迷い込む。<br>ふたりは、たくさんのお宝が眠るというこの伝説の大地で、<br>ずっと夢見てきたお宝探しの大冒険をスタートさせる！');
 
 
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (1,221);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (2,222);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (3,223);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (4,1);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (5,41);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (6,101);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (7,214);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (8,81);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (9,166);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (10,187);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (11,121);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (12,24);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (13,211);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (14,17);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (15,54);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (16,163);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (17,96);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (18,37);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (19,40);
+INSERT INTO ranking (ranking_id,shohin_id) VALUES (20,141);
 
-UPDATE ranking SET shohin_id = 221 WHERE ranking_id =  1;
-UPDATE ranking SET shohin_id = 222 WHERE ranking_id =  2;
-UPDATE ranking SET shohin_id = 223 WHERE ranking_id =  3;
-UPDATE ranking SET shohin_id = 1 WHERE ranking_id =  4;
-UPDATE ranking SET shohin_id = 41 WHERE ranking_id =  5;
-UPDATE ranking SET shohin_id = 101 WHERE ranking_id =  6;
-UPDATE ranking SET shohin_id = 214 WHERE ranking_id =  7;
-UPDATE ranking SET shohin_id = 81 WHERE ranking_id =  8;
-UPDATE ranking SET shohin_id = 166 WHERE ranking_id =  9;
-UPDATE ranking SET shohin_id = 187 WHERE ranking_id = 10;
-UPDATE ranking SET shohin_id = 121 WHERE ranking_id = 11;
-UPDATE ranking SET shohin_id = 24 WHERE ranking_id = 12;
-UPDATE ranking SET shohin_id = 211 WHERE ranking_id = 13;
-UPDATE ranking SET shohin_id = 17 WHERE ranking_id = 14;
-UPDATE ranking SET shohin_id = 54 WHERE ranking_id = 15;
-UPDATE ranking SET shohin_id = 163 WHERE ranking_id = 16;
-UPDATE ranking SET shohin_id = 96 WHERE ranking_id = 17;
-UPDATE ranking SET shohin_id = 37 WHERE ranking_id = 18;
-UPDATE ranking SET shohin_id = 40 WHERE ranking_id = 19;
-UPDATE ranking SET shohin_id = 141 WHERE ranking_id = 20;
 
 
